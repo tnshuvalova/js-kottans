412bd51cb5045f308d0147a7a95cc6c8eaad26b1
tnshuvalova <tshuvalova@cogniance.com>
2014-11-14 17:28:23 +0200

    html task

--- tasks/js_task_1/main.js
+++ tasks/js_task_1/main.js
@@ -10,13 +10,26 @@ var a = {
   }
 };
 
+var a = {
+  'p': 1,
+  'r': {
+    's': 't'
+  },
+  'u': {
+    'f': {
+      'z': 'x'
+    }
+  }
+};
+
 function deepCopy(o) {
   var tmp = {};
-  if(o!==null||typeof tmp != 'object')
+  if(o==null||typeof tmp != 'object')
     return o;
   else{
     for(var p in o){
-      tmp[p] = deepCopy(o[p]);
+      if(typeof o[p] == 'object') tmp[p] = deepCopy(o[p]);
+      else tmp[p] = o[p];
     }
   }
   return tmp;
@@ -26,7 +39,7 @@ function deepCopy2(o){
   return JSON.parse(JSON.stringify(o));
 }
 
-var b = deepCopy2(a);
+var b = deepCopy(a);
 a.p=2;
 console.log(b.p);
 
